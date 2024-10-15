8e36484cd09fefcee7bf993194a09bf92200900d
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-10-15 19:25:35 +0100

    Set up routes

--- app/main.py
+++ app/main.py
@@ -10,9 +10,6 @@ main_router = APIRouter()
 main_router.include_router(home_router)
 
 
-# https://stackoverflow.com/questions/71031816/how-do-you-properly-reuse-an-httpx-asyncclient-within-a-fastapi-application
-# https://medium.com/@benshearlaw/how-to-use-httpx-request-client-with-fastapi-16255a9984a4
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.requests_client = httpx.AsyncClient()
@@ -23,10 +20,12 @@ async def lifespan(app: FastAPI):
 # create app instance
 app = FastAPI(lifespan=lifespan)
 
-
 # add route for static files
 app.mount(
     "/static",
     StaticFiles(directory="app/static"),
     name="static",
 )
+
+# include routes in app
+app.include_router(main_router)
